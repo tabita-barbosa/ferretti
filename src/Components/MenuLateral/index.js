ec5b53b11e827ab2136d08d697ef0b48c38a36d9
Tabita Barbosa <tabita.barbosa8@gmail.com>
2020-08-24 09:28:15 -0300

    menu funciona, alinhando logo e icones

--- src/Components/MenuLateral/index.js
+++ src/Components/MenuLateral/index.js
@@ -1,65 +1,68 @@
 import React, { useState } from 'react';
-import { NavItem, NavLink, Navbar, Nav, Collapse, NavbarToggler, Button} from "reactstrap";
+import { NavItem, NavLink, Navbar, Nav, Collapse, NavbarToggler, NavbarBrand, Row, Col } from "reactstrap";
 import { Link, BrowserRouter as Router } from 'react-router-dom';
 import './styles.css';
 import Quinhentos from '../../icons/500px-brands.svg';
 import Twitter from '../../icons/twitter-brands.svg';
 import Instagram from '../../icons/instagram-brands.svg';
 import Facebook from '../../icons/facebook-f-brands.svg';
+import Logo from '../../assets/logo-provisorio.png';
 
 const MenuLateral = () => {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
 
 
     return (
         <Router>
-            <div className="container-nav">
-                <h1>Gui Ferretti</h1>
-                {/* <Navbar vertical expand="md"> */}
-                    {/* <Button color="primary" onClick={toggle}>
-                        MENU
-                    </Button> */}
-                    {/* <Collapse isOpen={isOpen}> */}
-                        <Nav className="d-flex justify-content-end">
-                            <NavItem>
-                                <NavLink tag={Link} to={"/"}>
-                                    Home
+            <div className="container-nav d-flex">
+                <div className="class-teste">
+                    <NavbarBrand>
+                        <img className="logo-imagem" alt="logo do fotografo" src={Logo}></img>
+                    </NavbarBrand>
+                    <Navbar light className="" expand="sm">
+                        <NavbarToggler onClick={toggle} />
+                        <Collapse isOpen={isOpen} navbar>
+                            <Nav vertical className="d-flex justify-content-end" navbar>
+                                <NavItem>
+                                    <NavLink tag={Link} to={"/"}>
+                                        Home
                             </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to={"/"}>
-                                    Portfólio
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to={"/"}>
+                                        Portfólio
                             </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to={"/"}>
-                                    Pacotes
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to={"/"}>
+                                        Pacotes
                             </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to={"/"}>
-                                    Equipe
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to={"/"}>
+                                        Equipe
                             </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to={"/"}>
-                                    Parceiros
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to={"/"}>
+                                        Parceiros
                             </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to={"/"}>
-                                    Contato
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to={"/"}>
+                                        Contato
                             </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to={"/"}>
-                                    Login
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to={"/"}>
+                                        Login
                             </NavLink>
-                            </NavItem>
-                        </Nav>
-                    {/* </Collapse> */}
-                {/* </Navbar> */}
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Navbar>
+                </div>
                 <div className=" div-icones d-flex justify-content-end">
                     <a href="https://www.instagram.com/guiferretti/">
                         <img alt="icone do instagram" className="icones" src={Instagram}></img>
